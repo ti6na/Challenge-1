6f372aaf9524541b07c1119771c2c78993758eb8
ti6na <55356082+ti6na@users.noreply.github.com>
2019-09-22 17:05:03 -0400

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+    public Text loseText;
+
+    private Rigidbody2D rb2d;
+    private int count;
+    private int lives;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        lives = 3;
+        winText.text = " ";
+        loseText.text = " ";
+        SetCountText();
+        SetLivesText();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKey("escape"))
+            Application.Quit();
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+        else if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetLivesText();
+        }
+        if (count == 11)
+        {
+            transform.position = new Vector2(32, 0);
+        }
+
+    }
+    void SetCountText()
+    {
+        countText.text = "blobs: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "great job. " +
+                "game created by Tiana George";
+        }
+    }
+
+    void SetLivesText()
+    {
+        livesText.text = "Lives: " + lives.ToString();
+        if (lives<1)
+        {
+            loseText.text = "you lost.";
+            Destroy(gameObject);
+        }
+    }
+}
